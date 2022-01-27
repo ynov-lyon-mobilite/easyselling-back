83b9f706a95806c1437388bd7610b5c94ac2edc6
Nicolas Barbosa <nicolas.barbosa@orange.com>
2022-01-27 14:51:46 +0100

    Send vehicle authorization id in email without error handling

--- extensions/endpoints/vehicles/index.js
+++ extensions/endpoints/vehicles/index.js
@@ -4,19 +4,22 @@ module.exports = function registerEndpoint(router, DirectusSdk) {
     const {PUBLIC_URL} = DirectusSdk.env;
 
     router.post('/:id/share', async (req, res, next) => {
-        console.log(DirectusSdk.exceptions);
         const vehicleId = req.params.id;
-        const receiverId = req.body.email;
+        const receiverMail = req.body.email;
 
         const vehicleService = new ItemsService('vehicles', {schema: req.schema, accountability: req.accountability});
+        const vehicleAuthorizationService = new ItemsService('vehicles_authorization', {schema: req.schema, accountability: req.accountability});
+        const userService = new ItemsService('directus_users', {schema: req.schema, accountability: req.accountability});
+        const mailService = new MailService({schema: req.schema});
         try {
-            const vehicle = await vehicleService.readOne(vehicleId)
-            const mailService = new MailService({schema: req.schema});
 
-            const url = `${PUBLIC_URL}/vehicles/share?test=test`
+            const user = await userService.readOne({ email: receiverMail })
+            const vehicle = await vehicleService.readOne(vehicleId)
+            const vehicleAuthorizationId = await vehicleAuthorizationService.createOne({ vehicle, user })
+            const url = `${PUBLIC_URL}/vehicles/share?id=${vehicleAuthorizationId}`
 
             await mailService.send({
-                to: receiverId,
+                to: receiverMail,
                 subject: "Partage de véhicule - EasySelling",
                 template: {
                     name: 'user-invitation',
