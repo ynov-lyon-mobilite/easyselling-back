bc2151b2243bac71e7cdc7ef11f7eb34d4a64070
MaxenceMottard <maxence.mottard@worldline.com>
2022-02-16 23:55:03 +0100

    add apple-app-site-association

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -4,4 +4,22 @@ import { AppService } from '../services/app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('apple-app-site-association')
+  appleAppSiteAssociation() {
+    return {
+      applinks: {
+        apps: [],
+        details: [
+          {
+            appID: '94FLWD5X32.com.ydays.easyselling',
+            paths: ['/admin/reset-password', '/vehicles/share'],
+          },
+        ],
+      },
+      webcredentials: {
+        apps: ['94FLWD5X32.com.ydays.easyselling'],
+      },
+    };
+  }
 }
