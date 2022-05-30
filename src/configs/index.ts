d056dea978c885ef2c505515c9be86378433060f
Maxence Mottard <maxence.mottard@worldline.com>
2022-05-30 17:08:24 +0200

    deploy to heroku

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,14 +1,14 @@
-import * as firebaseConfig from '../../serviceAccountKey.json';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('dotenv').config();
+}
 
 export default {
   port: process.env.PORT,
   mongoUrl: process.env.MONGO_URL,
   apiUrl: process.env.API_URL,
   firebase: {
-    credential: firebaseConfig,
+    credential: JSON.parse(process.env.FIREBASE_CREDENTIALS),
     apiKey: process.env.FIREBASE_API_KEY,
     bucketName: process.env.FIREBASE_BUCKET_NAME,
   },
